5bd4df590e08b7720543b10f09f4e1557d5c25f0
vm <mrkevinpro61@gmail.com>
2025-05-01 20:00:58 +0300

    Added test stuff.

--- MESH-code/src/appMain.c
+++ MESH-code/src/appMain.c
@@ -1,6 +1,7 @@
 //
 // Created by vm on 25.6.4.
 //
+#include <math.h>
 #include "appMain.h"
 #include "utils.h"
 #include "cc1101.h"
@@ -51,14 +52,14 @@ void appMain(ADC_HandleTypeDef *hadc,
     struct CCconfig config = {
             .mod=MOD_ASK_OOK,
             .freq=433.8,
-            .drate=10,
+            .drate=0.6,
             .power=10,
             .pktLenMode=PKT_LEN_MODE_VARIABLE,
             .packetMaxLen=255,
             .addrFilterMode=ADDR_FILTER_MODE_NONE,
             .syncMode=SYNC_MODE_16_16,
             .syncWord=0x6996,
-            .bandwidth=0,
+            .bandwidth=60,
             .crc=false,
             .preambleLen=16,
     };
@@ -79,23 +80,27 @@ void appMain(ADC_HandleTypeDef *hadc,
     interrupts[1].gpio = GPIO_PIN_15;
 
     cc->trState = RX_STOP;
-    cc->defaultState = DEF_RX;
     enableInterrupts = 1;
     // receiver
     uint32_t uid = HAL_GetUIDw2();
-
+//    cc1101_start_receive(cc);
+//    while (1){
+//        float rssidmb= cc1101_rssiToDbm(cc1101_getRssi(cc));
+//        int rssidbni=floor(rssidmb);
+//        int rssidbnf=floor((rssidmb-rssidbni)*100);
+//        printf("rssi: %d,%d\r\n",rssidbni, rssidbnf);
+//        HAL_Delay(100);
+//    }
     switch (uid) {
         case 0x3F004D:
-
             receiver();
-
             break;
         case 0x3C004C:
         case 0X280050:
-            transmitter();
             break;
-
     }
+    transmitter();
+
 
 
 
@@ -164,42 +169,50 @@ void appMain(ADC_HandleTypeDef *hadc,
 
 void on_receive(uint8_t *data, uint8_t len, uint8_t rssi, uint8_t lq) {
     HAL_GPIO_WritePin(GPIOA, D4, 1);
-    printf("received %d:", len);
-    for (int i = 0; i < len; i++) {
-        printf("%c", data[i]);
+    struct Packet packet = *((struct Packet *) data);
+    if (packet.checksum == (packet.id ^ *((uint32_t *) &packet.rssi))) {
+        float recRssi = cc1101_rssiToDbm(rssi);
+        int recrssidbni = floor(recRssi);
+        int recrssidbnf = floor((recRssi - recrssidbni) * 10);
+        printf("Received rssi: %3d.%1d | ", recrssidbni, recrssidbnf);
+        float prssi = packet.rssi;
+        int rssidbni = floor(prssi);
+        int rssidbnf = floor((prssi - rssidbni) * 10);
+        printf("id: %#08lX, rssi:%3d.%1d", packet.id, rssidbni, rssidbnf);
+        printf("\r\n");
     }
-    printf(" rssi: %d, lq: %d", rssi, lq);
-    printf("\r\n");
     //transmit answer
-    uint8_t transmit[256];
-    uint8_t size = sprintf(transmit,
-                           "123 %d",
-                           n);
-    cc1101_transmit_sync(cc, transmit, size + 1, 0);
-    n++;
     HAL_GPIO_WritePin(GPIOA, D4, 0);
 }
 
+void on_receive_empty(uint8_t *data, uint8_t len, uint8_t rssi, uint8_t lq) {
+    UNUSED(data);
+    UNUSED(len);
+    UNUSED(rssi);
+    UNUSED(lq);
+}
+
 void transmitter(void) {
-    uint8_t transmit[40];
-    n = 0;
+    cc->defaultState = DEF_RX;
+    cc1101_receiveCallback(cc, on_receive_empty);
+    cc1101_start_receive(cc);
+    struct Packet packet;
+    packet.id = HAL_GetUIDw2();
     while (1) {
         HAL_GPIO_WritePin(GPIOA, D3, 1);
-        uint8_t size = sprintf(transmit,
-                               "123123132123123132132132123132132132123132132123132132132132123132123132132132132132132132 %d",
-                               n);
-        cc1101_transmit_async(cc, transmit, size + 1, 0);
+        packet.rssi = cc1101_rssiToDbm(cc1101_getRssi(cc));
+        packet.checksum = packet.id ^ *((uint32_t *) &packet.rssi);
+        cc1101_transmit_sync(cc, (uint8_t *) &packet, sizeof(struct Packet), 0);
         HAL_GPIO_WritePin(GPIOA, D3, 0);
-        HAL_Delay(500);
-        n++;
+        HAL_Delay(200);
     }
 }
 
 void receiver(void) {
+    cc->defaultState = DEF_RX;
     cc1101_receiveCallback(cc, on_receive);
     cc1101_start_receive(cc);
     while (1) {
-
         HAL_Delay(100);
     }
 }
