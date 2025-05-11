dedccc44af20ec24286332b3b7051f620599209b
vm <mrkevinpro61@gmail.com>
2025-05-11 13:27:03 +0300

    Added math expression parsing library Added reduced printf uart transmission fix

--- MESH-code/src/appMain.c
+++ MESH-code/src/appMain.c
@@ -2,11 +2,13 @@
 // Created by vm on 25.6.4.
 //
 #include <math.h>
+#include <sys/unistd.h>
+#include <stdio.h>
 #include "appMain.h"
 #include "utils.h"
 #include "cc1101.h"
-#include "usbd_cdc_if.h"
 #include "hw.h"
+#include "expr.h"
 
 #define D3 GPIO_PIN_15
 #define D4 GPIO_PIN_1
@@ -19,49 +21,88 @@ void appMain(ADC_HandleTypeDef *hadc,
              TIM_HandleTypeDef *htim2,
              TIM_HandleTypeDef *htim6,
              UART_HandleTypeDef *huart2) {
-    //init utilities
+
+    HAL_Delay(2000);
+    printf("%#08lX\r\n", HAL_GetUIDw2());
+    //    //init utilities
     hw_enable_ld(true);
     utils_init(htim6);
-    HAL_Delay(1000);
+//
 
-    /*
-    * stm32 has 96 bit large unique id which consists of
-    * UID[31:0]:  X and Y coordinates on the wafer expressed in BCD format
-    * UID[39:32]: WAF_NUM[7:0] Wafer number (8-bit unsigned number)
-    * UID[63:40]: LOT_NUM[23:0] Lot number (ASCII encoded)
-    * UID[95:64]: LOT_NUM[55:24] Lot number (ASCII encoded)
-    * 96 bits are equally split among 3 uint32
-    * Because of that and the fact that all 10 chips are from same batch, first 2 uint32 are the same,
-    * and does not require check
-    * example:
-    * 0  256324400
-    * 1  909523256
-    * 2  4128845
-    * 0  256324400
-    * 1  909523256
-    * 2  4128848
-    */
-    printf("%#08lX\r\n", HAL_GetUIDw2());
-    printf("%#08lX\r\n", HAL_GetUIDw2());
-    printf("%#08lX\r\n", HAL_GetUIDw2());
+    struct mallinfo mi = mallinfo();
+    printf("Total non-mmapped bytes (arena):       %d\n", mi.arena);
+    printf("Total allocated space (uordblks):      %d\n", mi.uordblks);
+    printf("Total free space (fordblks):           %d\n", mi.fordblks);
 
-    uint8_t data_buffer[1000] = {0};
-    HAL_UART_Receive_DMA(huart2, data_buffer, 1000);
-    HAL_Delay(0);
-    uint8_t data_start[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x04, 0x00, 0xFF, 0x00,  0x01,0x00,0x04, 0x03, 0x02, 0x01};
-    uint8_t data_config[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x02, 0x00, 0x62, 0x00,  0x04, 0x03, 0x02, 0x01};
-    HAL_UART_Transmit(huart2, data_start, 14, 5000);
-    HAL_UART_Transmit(huart2, data_config, 12, 5000);
-//    uint8_t data_end[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x02, 0x00, 0xFE, 0x00, 0x04, 0x03, 0x02, 0x01};
-//    HAL_UART_Transmit(huart2, data_end, 12, 5000);
-//    HAL_UART_Transmit(huart2, data_end, 12, 100);
-//    HAL_UART_Receive(huart2,data_buffer,200,5000);
+//    expr_test();
+    char *s = "x*2**x**2**x**2**x";
+    struct expr_var_list vars = {0};
+    static struct expr_func user_funcs[] = {
+            {NULL, NULL, NULL, 0},
+    };
+    struct expr *e = expr_create(s, strlen(s), &vars, user_funcs);
+
+    struct expr_var *varx = expr_var(&vars, "x", 1);
+    varx->value = 1;
+    if (e == NULL) {
+        printf("FAIL: %s returned NULL\n", s);
+        return;
+    }
+    float result = expr_eval(e);
+    printf("result = %f\n", result);
+
+    expr_destroy(e, &vars);
+
+    mi = mallinfo();
+    printf("Total non-mmapped bytes (arena):       %d\n", mi.arena);
+    printf("Total allocated space (uordblks):      %d\n", mi.uordblks);
+    printf("Total free space (fordblks):           %d\n", mi.fordblks);
+
+
+//
+//    /*
+//    * stm32 has 96 bit large unique id which consists of
+//    * UID[31:0]:  X and Y coordinates on the wafer expressed in BCD format
+//    * UID[39:32]: WAF_NUM[7:0] Wafer number (8-bit unsigned number)
+//    * UID[63:40]: LOT_NUM[23:0] Lot number (ASCII encoded)
+//    * UID[95:64]: LOT_NUM[55:24] Lot number (ASCII encoded)
+//    * 96 bits are equally split among 3 uint32
+//    * Because of that and the fact that all 10 chips are from same batch, first 2 uint32 are the same,
+//    * and does not require check
+//    * example:
+//    * 0  256324400
+//    * 1  909523256
+//    * 2  4128845
+//    * 0  256324400
+//    * 1  909523256
+//    * 2  4128848
+//    */
+    printf("%#08lX\r\n", HAL_GetUIDw2());
+    printf("%#08lX\r\n", HAL_GetUIDw2());
+    printf("%#08lX\r\n", HAL_GetUIDw2());
+//
+    uint8_t data_buffer[500] = {0};
+////    HAL_UART_Receive_DMA(huart2, data_buffer, 1000);
+
+    // transmitting 0 fixed bug
+    HAL_UART_Transmit(huart2, data_buffer, 1, 100);
+//
+    uint8_t data_start[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x04, 0x00, 0xFF, 0x00, 0x01, 0x00, 0x04, 0x03, 0x02, 0x01};
+//    uint8_t data_config[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x02, 0x00, 0x62, 0x00,  0x04, 0x03, 0x02, 0x01};
+    HAL_UART_Transmit(huart2, data_start, 14, 100);
+////    HAL_UART_Transmit(huart2, data_start, 14, 5000);
+//
+////    HAL_UART_Transmit(huart2, data_config, 12, 5000);
+////    uint8_t data_end[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x02, 0x00, 0xFE, 0x00, 0x04, 0x03, 0x02, 0x01};
+////    HAL_UART_Transmit(huart2, data_end, 12, 5000);
+////    HAL_UART_Transmit(huart2, data_end, 12, 100);
+    HAL_UART_Receive(huart2, data_buffer, 20, 10);
     HAL_Delay(5000);
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 500; i++) {
         printf("%x ", data_buffer[i]);
     }
     printf("\t\n");
-//    printf("s1:%d|s2:%d",s1);
+////    printf("s1:%d|s2:%d",s1);
 
 
 
